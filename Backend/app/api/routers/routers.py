862c9a494809d34ebf08a8e19c5455777887ae0a
EstrNous <praduntsev@yandex.ru>
2025-10-18 03:51:56 +0400

    Залинкованные ручки  !! Осторожно!! Без сессий вызывают Баги

--- Backend/app/api/routers/routers.py
+++ Backend/app/api/routers/routers.py
@@ -1,16 +1,18 @@
 import os
 import httpx
 from sqlalchemy.orm import Session
-from fastapi import APIRouter, HTTPException, status, WebSocket
+from fastapi import APIRouter, HTTPException, status
 from dotenv import load_dotenv
 from Backend.app.schemas import PromptRequest, SimpleAnswer, SupportRequest
 from Backend.app.services import simulation_manager
+from Backend.crud.base_crud import get_ticket_times, get_tickets_by_status
 
 # Загружаем переменные окружения
 load_dotenv()
 
 r = APIRouter()
 
+db = Session # Здесь будет ссылка на сесиию постгре из .env преположительно
 ML_API_URL = os.getenv('ML_API_URL')
 
 @r.post("/test-ml", response_model=SimpleAnswer)
@@ -59,12 +61,16 @@ async def simulate_status():
     return simulation_manager.status()
 
 @r.get("/statistic/all_count/{status_t}")
-async def get_sum_of_tickets(status_t: str):
-    return {"message": "Обратился к Витале c"+ status_t}
+async def get_sum_of_tickets(status_t: str, db = db):
+    return get_tickets_by_status(db, status_t).count()
 
 @r.get("/statistic/time_spending")
-async def spend_time():
+async def spend_time(db=db):
     time = []
-    for i in range(10):
-        time.append(i)
+    for i in range(get_tickets_by_status(db, "all").count()):
+        time.append(get_ticket_times(db, i).get("resolved_at") - get_ticket_times(db, i).get("created_at"))
     return sum(time)/ len(time)
+
+@r.get("/statistic/cards/{status}")
+async def get_tickets(status_t: str, db = db):
+    return get_tickets_by_status(db, status_t)
